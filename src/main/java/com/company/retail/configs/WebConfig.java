43b3628c21267c124ab4d5f6602bf15941594c86
tentenpanashe01 <149800126+tentenpanashe01@users.noreply.github.com>
2025-11-17 11:30:12 +0200

    Update CORS allowed origins in WebConfig

--- src/main/java/com/company/retail/configs/WebConfig.java
+++ src/main/java/com/company/retail/configs/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://retailfront.netlify.app","http://localhost: 5173")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
